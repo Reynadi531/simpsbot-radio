b567fd345f4f77b7726158f0111c4f1a8105211e
Reynadi Andreas Priesley <yerpa531@gmail.com>
2021-02-27 08:28:17 +0000

    🎇 Add status and procfile

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,8 +1,10 @@
 import { RunFunction } from '../../types/event';
 import { Bot } from '../../client';
-
+import { config } from 'dotenv';
+config();
 const name: string = 'ready';
 const run: RunFunction = async(client: Bot) => {
+    client.user.setActivity(process.env.status)
     client.logger.success(`Logged in as ${client.user.tag}`);
 }
 
