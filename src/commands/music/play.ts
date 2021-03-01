3525aaca5b0c9c825e55e758bd5f8f61c35d17b8
Reynadi Andreas Priesley <yerpa531@gmail.com>
2021-03-01 07:16:17 +0000

    remove permission

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -13,19 +13,19 @@ const run: RunFunction = async(client, message, args) => {
         // @ts-ignore
         const connection = await client.channels.cache.get(client.currentVC).join();
             const player = (url) => {
+                if(!client.playlist[0].videos) return message.channel.send('No playlist queue')
                 // @ts-ignore
-            client.dispatcher = connection.play(ytdl(url))
-                .on('finish', () => {
-                    if(!client.playlist[0].videos) return message.channel.send('No playlist queue')
-                    if(client.currentPlayCount == client.playlist[0].videos.length) {
-                        // console.log(client.currentPlayCount, client.playlist[0].videos.length)
-                        client.currentPlayCount = 1;
-                    } else {
-                        client.currentPlayCount += 1;
-                    }
-                    player(client.playlist[0].videos[client.currentPlayCount - 1]);
-                })
-                .on('error', error => console.error(error));
+                client.dispatcher = connection.play(ytdl(url))
+                    .on('finish', () => {
+                        if(client.currentPlayCount == client.playlist[0].videos.length) {
+                            // console.log(client.currentPlayCount, client.playlist[0].videos.length)
+                            client.currentPlayCount = 1;
+                        } else {
+                            client.currentPlayCount += 1;
+                        }
+                        player(client.playlist[0].videos[client.currentPlayCount - 1]);
+                    })
+                    .on('error', error => console.error(error));
             }
             player(client.playlist[0].videos[client.currentPlayCount - 1]);
         } catch (error) {
--- src/commands/music/setvc.ts
+++ src/commands/music/setvc.ts
@@ -6,7 +6,6 @@ const name: string = 'setvc';
 const description: string = 'Set the voice channel';
 const category: string = 'music';
 const run: RunFunction = async(client, message, args) => {
-    if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You need at least manage channels permission');
     if(!args[0] && isNaN(Number(args[0]))) return message.channel.send(`Require arguments | ${client.Config.prefix}${name} <voice channel id>`);
     try {
         const channel = client.channels.cache.get(args[0]) || await client.channels.fetch(args[0]);
